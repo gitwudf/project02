d4c6773e5324ea789981d79dda261b0168bcef82
tom_glb <goodMorning_glb@qq.com>
2018-10-06 10:44:27 +0800

    1111

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("AAAAAAAAAAAAAA");
+        System.out.println("11111111111111");
     }
 }
