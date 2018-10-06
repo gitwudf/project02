922ef88f0060dbc8e35692193bfbfd515e13f9a2
tom_glb <goodMorning_glb@qq.com>
2018-10-06 09:36:39 +0800

    Initial commit

---
+++ javaMoudle/src/com/henu/entity/Student.java
@@ -0,0 +1,18 @@
+package com.henu.entity;
+
+/**
+ * 作者 吴登峰
+ * 创建时间:2018/10/6
+ */
+public class Student {
+
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private String tel;
+
+    
+}
