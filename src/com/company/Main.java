5eeea83e78667fcb56df99fb45f1687bcc47539f
tom_glb <goodMorning_glb@qq.com>
2018-10-06 11:08:30 +0800

    333333

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("11111111111111");
+        System.out.println("4333333333333");
     }
+
+    private void add(){}
 }
